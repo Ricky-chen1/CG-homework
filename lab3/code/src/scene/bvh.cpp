165d88d606b98786791467e96e0c972b7d5122a6
Ricky-chen1 <Ricky__chen@outlook.com>
2024-11-06 12:48:02 +0800

    complete 2.1

--- lab3/code/src/scene/bvh.cpp
+++ lab3/code/src/scene/bvh.cpp
@@ -8,107 +8,159 @@
 
 using namespace std;
 
-namespace CGL {
-namespace SceneObjects {
+namespace CGL
+{
+  namespace SceneObjects
+  {
 
-BVHAccel::BVHAccel(const std::vector<Primitive *> &_primitives,
-                   size_t max_leaf_size) {
+    BVHAccel::BVHAccel(const std::vector<Primitive *> &_primitives,
+                       size_t max_leaf_size)
+    {
 
-  primitives = std::vector<Primitive *>(_primitives);
-  root = construct_bvh(primitives.begin(), primitives.end(), max_leaf_size);
-}
-
-BVHAccel::~BVHAccel() {
-  if (root)
-    delete root;
-  primitives.clear();
-}
-
-BBox BVHAccel::get_bbox() const { return root->bb; }
-
-void BVHAccel::draw(BVHNode *node, const Color &c, float alpha) const {
-  if (node->isLeaf()) {
-    for (auto p = node->start; p != node->end; p++) {
-      (*p)->draw(c, alpha);
+      primitives = std::vector<Primitive *>(_primitives);
+      root = construct_bvh(primitives.begin(), primitives.end(), max_leaf_size);
     }
-  } else {
-    draw(node->l, c, alpha);
-    draw(node->r, c, alpha);
-  }
-}
 
-void BVHAccel::drawOutline(BVHNode *node, const Color &c, float alpha) const {
-  if (node->isLeaf()) {
-    for (auto p = node->start; p != node->end; p++) {
-      (*p)->drawOutline(c, alpha);
+    BVHAccel::~BVHAccel()
+    {
+      if (root)
+        delete root;
+      primitives.clear();
     }
-  } else {
-    drawOutline(node->l, c, alpha);
-    drawOutline(node->r, c, alpha);
-  }
-}
 
-BVHNode *BVHAccel::construct_bvh(std::vector<Primitive *>::iterator start,
-                                 std::vector<Primitive *>::iterator end,
-                                 size_t max_leaf_size) {
+    BBox BVHAccel::get_bbox() const { return root->bb; }
 
-  // TODO (Part 2.1):
-  // Construct a BVH from the given vector of primitives and maximum leaf
-  // size configuration. The starter code build a BVH aggregate with a
-  // single leaf node (which is also the root) that encloses all the
-  // primitives.
+    void BVHAccel::draw(BVHNode *node, const Color &c, float alpha) const
+    {
+      if (node->isLeaf())
+      {
+        for (auto p = node->start; p != node->end; p++)
+        {
+          (*p)->draw(c, alpha);
+        }
+      }
+      else
+      {
+        draw(node->l, c, alpha);
+        draw(node->r, c, alpha);
+      }
+    }
 
+    void BVHAccel::drawOutline(BVHNode *node, const Color &c, float alpha) const
+    {
+      if (node->isLeaf())
+      {
+        for (auto p = node->start; p != node->end; p++)
+        {
+          (*p)->drawOutline(c, alpha);
+        }
+      }
+      else
+      {
+        drawOutline(node->l, c, alpha);
+        drawOutline(node->r, c, alpha);
+      }
+    }
 
-  BBox bbox;
+    BVHNode *BVHAccel::construct_bvh(std::vector<Primitive *>::iterator start,
+                                     std::vector<Primitive *>::iterator end,
+                                     size_t max_leaf_size)
+    {
 
-  for (auto p = start; p != end; p++) {
-    BBox bb = (*p)->get_bbox();
-    bbox.expand(bb);
-  }
+      // TODO (Part 2.1):
+      // Construct a BVH from the given vector of primitives and maximum leaf
+      // size configuration. The starter code build a BVH aggregate with a
+      // single leaf node (which is also the root) that encloses all the
+      // primitives.
 
-  BVHNode *node = new BVHNode(bbox);
-  node->start = start;
-  node->end = end;
+      BBox bbox;
+      // 当前包围盒图元数
+      size_t count = 0;
 
-  return node;
+      // 获得整个包围盒
+      for (auto p = start; p != end; p++)
+      {
+        BBox bb = (*p)->get_bbox();
+        bbox.expand(bb);
+        count++;
+      }
 
+      BVHNode *node = new BVHNode(bbox);
+      node->start = start;
+      node->end = end;
 
-}
+      // 选择分割轴
+      Vector3D extent = bbox.extent;
+      int axis;
+      if (extent.x >= extent.y && extent.x >= extent.z)
+        axis = 0; // x 轴
+      else if (extent.y >= extent.z)
+        axis = 1; // y 轴
+      else
+        axis = 2; // z 轴
 
-bool BVHAccel::has_intersection(const Ray &ray, BVHNode *node) const {
-  // TODO (Part 2.3):
-  // Fill in the intersect function.
-  // Take note that this function has a short-circuit that the
-  // Intersection version cannot, since it returns as soon as it finds
-  // a hit, it doesn't actually have to find the closest hit.
+      // 计算图元的平均质心（用于分割）
+      double split_point = 0;
+      for (auto p = start; p != end; p++)
+      {
+        split_point += (*p)->get_bbox().centroid()[axis];
+      }
+      split_point /= count;
 
+      // leaf node
+      if (count <= max_leaf_size)
+      {
+        node->l = nullptr;
+        node->r = nullptr;
+        return node;
+      }
+      // internal node
+      // 划分左右
+      auto mid = std::partition(start, end, [axis, split_point](Primitive *prim)
+                                { return prim->get_bbox().centroid()[axis] < split_point; });
 
+      // 无法根据质心划分时
+      if (mid == start || mid == end)
+      {
+        mid = start + count / 2;
+      }
 
-  for (auto p : primitives) {
-    total_isects++;
-    if (p->has_intersection(ray))
-      return true;
-  }
-  return false;
+      node->l = construct_bvh(start, mid, max_leaf_size);
+      node->r = construct_bvh(mid, end, max_leaf_size);
 
+      return node;
+    }
 
-}
+    bool BVHAccel::has_intersection(const Ray &ray, BVHNode *node) const
+    {
+      // TODO (Part 2.3):
+      // Fill in the intersect function.
+      // Take note that this function has a short-circuit that the
+      // Intersection version cannot, since it returns as soon as it finds
+      // a hit, it doesn't actually have to find the closest hit.
 
-bool BVHAccel::intersect(const Ray &ray, Intersection *i, BVHNode *node) const {
-  // TODO (Part 2.3):
-  // Fill in the intersect function.
+      for (auto p : primitives)
+      {
+        total_isects++;
+        if (p->has_intersection(ray))
+          return true;
+      }
+      return false;
+    }
 
+    bool BVHAccel::intersect(const Ray &ray, Intersection *i, BVHNode *node) const
+    {
+      // TODO (Part 2.3):
+      // Fill in the intersect function.
 
+      bool hit = false;
+      for (auto p : primitives)
+      {
+        total_isects++;
+        hit = p->intersect(ray, i) || hit;
+      }
+      return hit;
+    }
 
-  bool hit = false;
-  for (auto p : primitives) {
-    total_isects++;
-    hit = p->intersect(ray, i) || hit;
-  }
-  return hit;
-
-
-}
-
-} // namespace SceneObjects
+  } // namespace SceneObjects
 } // namespace CGL
--- lab3/code/src/scene/triangle.cpp
+++ lab3/code/src/scene/triangle.cpp
@@ -3,109 +3,119 @@
 #include "CGL/CGL.h"
 #include "GL/glew.h"
 
-namespace CGL {
-namespace SceneObjects {
+namespace CGL
+{
+  namespace SceneObjects
+  {
 
-Triangle::Triangle(const Mesh *mesh, size_t v1, size_t v2, size_t v3) {
-  p1 = mesh->positions[v1];
-  p2 = mesh->positions[v2];
-  p3 = mesh->positions[v3];
-  n1 = mesh->normals[v1];
-  n2 = mesh->normals[v2];
-  n3 = mesh->normals[v3];
-  bbox = BBox(p1);
-  bbox.expand(p2);
-  bbox.expand(p3);
+    Triangle::Triangle(const Mesh *mesh, size_t v1, size_t v2, size_t v3)
+    {
+      p1 = mesh->positions[v1];
+      p2 = mesh->positions[v2];
+      p3 = mesh->positions[v3];
+      n1 = mesh->normals[v1];
+      n2 = mesh->normals[v2];
+      n3 = mesh->normals[v3];
+      bbox = BBox(p1);
+      bbox.expand(p2);
+      bbox.expand(p3);
 
-  bsdf = mesh->get_bsdf();
-}
+      bsdf = mesh->get_bsdf();
+    }
 
-BBox Triangle::get_bbox() const { return bbox; }
+    BBox Triangle::get_bbox() const { return bbox; }
 
-bool Triangle::has_intersection(const Ray &r) const {
-  // Part 1, Task 3: implement ray-triangle intersection
-  // The difference between this function and the next function is that the next
-  // function records the "intersection" while this function only tests whether
-  // there is a intersection.
-  // 向量应该逆时针顺序
-  Vector3D ab = p2 - p1;
-  Vector3D ac = p1 - p3; // 不应该为p3 - p1
-  Vector3D bc = p3 - p2;
-  double area = cross(ab,ac).norm() / 2;
-  Vector3D face_normal = cross(ab, ac).unit();
-  // 计算 t
-  double t = dot(p1 - r.o,face_normal) / dot(r.d,face_normal);
+    bool Triangle::has_intersection(const Ray &r) const
+    {
+      // Part 1, Task 3: implement ray-triangle intersection
+      // The difference between this function and the next function is that the next
+      // function records the "intersection" while this function only tests whether
+      // there is a intersection.
+      // 向量应该逆时针顺序
+      Vector3D ab = p2 - p1;
+      Vector3D ac = p1 - p3; // 不应该为p3 - p1
+      Vector3D bc = p3 - p2;
+      double area = cross(ab, ac).norm() / 2;
+      Vector3D face_normal = cross(ab, ac).unit();
+      // 计算 t
+      double t = dot(p1 - r.o, face_normal) / dot(r.d, face_normal);
 
-  if (t < r.min_t || t > r.max_t) { // 检查 t 是否有效
-    return false;
-  }
+      if (t < r.min_t || t > r.max_t)
+      { // 检查 t 是否有效
+        return false;
+      }
 
-  // 判断交点是否在三角形内
-  Vector3D p = r.o + t * r.d;
-  Vector3D ap = p - p1;
-  Vector3D bp = p - p2;
-  Vector3D cp = p - p3;
+      // 判断交点是否在三角形内
+      Vector3D p = r.o + t * r.d;
+      Vector3D ap = p - p1;
+      Vector3D bp = p - p2;
+      Vector3D cp = p - p3;
 
-  // 计算叉积
-  double cross1 = cross(ab, ap).z;
-  double cross2 = cross(ac, cp).z;
-  double cross3 = cross(bc, bp).z;
+      // 计算叉积
+      double cross1 = cross(ab, ap).z;
+      double cross2 = cross(ac, cp).z;
+      double cross3 = cross(bc, bp).z;
 
-  if ((cross1 >= 0 && cross2 >= 0 && cross3 >= 0) || 
-      (cross1 <= 0 && cross2 <= 0 && cross3 <= 0)) {
-    return true;
-  }
+      if ((cross1 >= 0 && cross2 >= 0 && cross3 >= 0) ||
+          (cross1 <= 0 && cross2 <= 0 && cross3 <= 0))
+      {
+        return true;
+      }
 
-  return false;
-}
+      return false;
+    }
 
-bool Triangle::intersect(const Ray &r, Intersection *isect) const {
-  // Part 1, Task 3:
-  // implement ray-triangle intersection. When an intersection takes
-  // place, the Intersection data should be updated accordingly
-  if (!has_intersection(r)){
-    return false;
-  }
+    bool Triangle::intersect(const Ray &r, Intersection *isect) const
+    {
+      // Part 1, Task 3:
+      // implement ray-triangle intersection. When an intersection takes
+      // place, the Intersection data should be updated accordingly
+      if (!has_intersection(r))
+      {
+        return false;
+      }
 
-  Vector3D ab = p2 - p1;
-  Vector3D ac = p3 - p1;
-  Vector3D bc = p3 - p2;
-  double area = cross(ab,ac).norm() / 2;
-  Vector3D face_normal = cross(ab, ac).unit();
-  double t = dot(r.o - p1,face_normal) / dot(r.d,face_normal);
-  Vector3D p = r.o + t * r.d;
-  Vector3D ap = p - p1;
-  Vector3D bp = p - p2;
-  Vector3D cp = p - p3;
+      Vector3D ab = p2 - p1;
+      Vector3D ac = p3 - p1;
+      Vector3D bc = p3 - p2;
+      double area = cross(ab, ac).norm() / 2;
+      Vector3D face_normal = cross(ab, ac).unit();
+      double t = dot(r.o - p1, face_normal) / dot(r.d, face_normal);
+      Vector3D p = r.o + t * r.d;
+      Vector3D ap = p - p1;
+      Vector3D bp = p - p2;
+      Vector3D cp = p - p3;
 
-  double alpha = cross(ap, bp).norm() / 2 / area;
-  double beta = cross(ap, cp).norm() / 2 / area;
-  double gama = cross(bp, cp).norm() / 2 / area;
+      double alpha = cross(ap, bp).norm() / 2 / area;
+      double beta = cross(ap, cp).norm() / 2 / area;
+      double gama = cross(bp, cp).norm() / 2 / area;
 
-  isect->bsdf = get_bsdf();
-  isect->t = t;
-  isect->primitive = this;
-  isect->n = (alpha * n1 + beta * n2 + (1 - alpha - beta) * n3).unit();
-  return true;
-}
+      isect->bsdf = get_bsdf();
+      isect->t = t;
+      isect->primitive = this;
+      isect->n = (alpha * n1 + beta * n2 + (1 - alpha - beta) * n3).unit();
+      return true;
+    }
 
-void Triangle::draw(const Color &c, float alpha) const {
-  glColor4f(c.r, c.g, c.b, alpha);
-  glBegin(GL_TRIANGLES);
-  glVertex3d(p1.x, p1.y, p1.z);
-  glVertex3d(p2.x, p2.y, p2.z);
-  glVertex3d(p3.x, p3.y, p3.z);
-  glEnd();
-}
+    void Triangle::draw(const Color &c, float alpha) const
+    {
+      glColor4f(c.r, c.g, c.b, alpha);
+      glBegin(GL_TRIANGLES);
+      glVertex3d(p1.x, p1.y, p1.z);
+      glVertex3d(p2.x, p2.y, p2.z);
+      glVertex3d(p3.x, p3.y, p3.z);
+      glEnd();
+    }
 
-void Triangle::drawOutline(const Color &c, float alpha) const {
-  glColor4f(c.r, c.g, c.b, alpha);
-  glBegin(GL_LINE_LOOP);
-  glVertex3d(p1.x, p1.y, p1.z);
-  glVertex3d(p2.x, p2.y, p2.z);
-  glVertex3d(p3.x, p3.y, p3.z);
-  glEnd();
-}
+    void Triangle::drawOutline(const Color &c, float alpha) const
+    {
+      glColor4f(c.r, c.g, c.b, alpha);
+      glBegin(GL_LINE_LOOP);
+      glVertex3d(p1.x, p1.y, p1.z);
+      glVertex3d(p2.x, p2.y, p2.z);
+      glVertex3d(p3.x, p3.y, p3.z);
+      glEnd();
+    }
 
-} // namespace SceneObjects
+  } // namespace SceneObjects
 } // namespace CGL
